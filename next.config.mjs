39414311d0d8c8bc3a73366885cfb7d7b8481f8b
Kirito-012 <11.uknowwho@gmail.com>
2025-11-23 20:47:35 +0530

    updated_new

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
-  /* config options here */
-  reactCompiler: true,
-};
+	images: {
+		domains: ['res.cloudinary.com'],
+	},
+	/* config options here */
+	reactCompiler: true,
+	output: 'export',
+}
 
-export default nextConfig;
+export default nextConfig
